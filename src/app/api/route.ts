02eb5c77aa3c0d663916cbf0bcbb18c5e6e489cf
mo-renike <herroyalpianist@gmail.com>
2023-06-26 04:38:25 +0100

    code cleanup

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,32 +1,32 @@
-import { NextResponse } from "next/server";
-import { headers } from "next/headers";
+// import { NextResponse } from "next/server";
+// import { headers } from "next/headers";
 
-export async function GET(request: {
-  query: { latitude: any; longitude: any };
-}) {
-  try {
-    const API_KEY = "AIzaSyBiVr3N5E4oa0pBJ8Q8m64UFBk5M0JtdXw";
-    const { latitude, longitude } = request.query;
-    const DATA_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=50000&type=hospital&keyword=hospital&key=${API_KEY}`;
+// export async function GET(request: {
+//   query: { latitude: any; longitude: any };
+// }) {
+//   try {
+//     const API_KEY = "AIzaSyBiVr3N5E4oa0pBJ8Q8m64UFBk5M0JtdXw";
+//     const { latitude, longitude } = request.query;
+//     const DATA_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=50000&type=hospital&keyword=hospital&key=${API_KEY}`;
 
-    const res = await fetch(DATA_URL, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-    });
+//     const res = await fetch(DATA_URL, {
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+//         "Access-Control-Allow-Headers": "Content-Type, Authorization",
+//       },
+//     });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch hospitals");
-    }
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch hospitals");
+//     }
 
-    const hospitals: Hospital[] = await res.json();
-    console.log(hospitals, "hospitals");
+//     const hospitals: Hospital[] = await res.json();
+//     console.log(hospitals, "hospitals");
 
-    return NextResponse.json({ hospitals });
-  } catch (error) {
-    console.error("Error fetching hospitals:", error);
-    return NextResponse.error();
-  }
-}
+//     return NextResponse.json({ hospitals });
+//   } catch (error) {
+//     console.error("Error fetching hospitals:", error);
+//     return NextResponse.error();
+//   }
+// }
